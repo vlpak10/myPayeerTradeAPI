2c834c1363990ce9717b02e11c645807c8359575
vlpak <vlpak1001@gmail.com>
2022-06-10 16:57:52 +0300

    add src/Api.php

--- src/Api.php
+++ src/Api.php
@@ -1,55 +1,56 @@
 <?php
     
+use Payeer\TradeApiPrototype\Payeer;
+    
 class Api {
-    private $name;
-    private $scope;
-    private $credentialsPath;
-    private $tokenPath;
+    
+    private $api;
 
-    public function __construct(array $params)
-    {
-        if (!$params['credentials']) {
-          throw new Exception("Path to credentials is required");
-        }
-        $this->credentialsPath = $params['credentials'];
-
-        if (!$params['token']) {
-          throw new Exception("Path to token is required");
-        }
-        $this->tokenPath = $params['token'];
-        $this->name = $params['name'] ?? 'Gmail API PHP';
-        $this->scope = $params['scope'] ?? Google_Service_Gmail::GMAIL_READONLY;
+    public function __construct(?string $apiId = null, ?string $apiSecret = null) {
+        $this->api = new Payeer($apiId, $apiSecret);
     }
 
-    public function getService()
-    {
-        $client = self::getClient();
-        return new Google_Service_Gmail($client);
+    public function getInfo($pair) {
+        return $this->api->getInfo($pair);
     }
     
-    private function makeNewToken(Google_Client $client)
-    {
-        // Request authorization from the user.
-        $authUrl = $client->createAuthUrl();
-        printf("Open the following link in your browser:\n%s\n", $authUrl);
-        print 'Enter verification code: ';
-        $authCode = trim(fgets(STDIN));
-
-        // Exchange authorization code for an access token.
-        $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
-        $client->setAccessToken($accessToken);
-
-        // Check to see if there was an error.
-        if (array_key_exists('error', $accessToken)) {
-          throw new Exception(join(', ', $accessToken));
-        }
+    public function getTicker($pair) {
+        return $this->api->getTicker($pair);
     }
-
-    private function saveToken(Google_Client $client)
-    {
-        if (!file_exists(dirname($this->tokenPath))) {
-          mkdir(dirname($this->tokenPath), 0700, true);
-        }
-        file_put_contents($this->tokenPath, json_encode($client->getAccessToken()));
+    
+    public function getOrders($pair) {
+        return $this->api->getOrders($pair);
+    }
+    
+    public function getTrades($pair) {
+        return $this->api->getTrades($pair);
+    }
+    
+    public function getAccount() {
+        return $this->api->getAccount();
+    }
+    
+    public function createOrder(array $params = []) {
+        return $this->api->createOrder($params);
+    }
+    
+    public function getOrderStatus(array $params = []) {
+        return $this->api->getOrderStatus($params);
+    }
+    
+    public function cancelOrder(array $params = []) {
+        return $this->api->cancelOrder($params);
+    }
+    
+    public function cancelOrders(array $params = []) {
+        return $this->api->cancelOrders($params);
+    }
+    
+    public function getMyOrders(array $params = []) {
+        return $this->api->getMyOrders($params);
+    }
+    
+    public function getMyTrades(array $params = []) {
+        return $this->api->getMyTrades($params);
     }
 }
